402f4b13b9126d3d2cfafa7af76feddf961d243e
Vishvajit Kher <vishvajit79@gmail.com>
2017-10-10 21:12:19 -0400

    Initial commit

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    //Public variables
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float startX;
+    [SerializeField]
+    private float endX;
+
+    //private variables
+    private Transform _transform;
+    private Vector2 _currentPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        _transform = gameObject.GetComponent<Transform>();
+        _currentPos = _transform.position;
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentPos = _transform.position;
+        //move ocean down
+        _currentPos -= new Vector2(speed, 0);
+
+        //check if we need to reset
+        if (_currentPos.x < endX)
+        {
+            //reset
+            Reset();
+        }
+        //apply changes
+        _transform.position = _currentPos;
+
+    }
+
+
+    private void Reset()
+    {
+
+        _currentPos = new Vector2(startX, 0);
+    }
+}
